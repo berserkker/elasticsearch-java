f8b8314f1054bae58443af5f91c556b46e969f03
justxgy <justxgy@outlook.com>
2018-09-17 19:37:58 +0800

    增加日志配置，多环境配置

--- src/main/java/com/xgy/elasticsearchjava/service/AsyncTaskService.java
+++ src/main/java/com/xgy/elasticsearchjava/service/AsyncTaskService.java
@@ -1,6 +1,8 @@
 package com.xgy.elasticsearchjava.service;
 
 import com.xgy.elasticsearchjava.entity.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -8,18 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 //任务执行类
 public class AsyncTaskService {
+    private static final Logger logger = LoggerFactory.getLogger(AsyncTaskService.class);
     @Autowired
     private OperatorImpl operatorImpl;
 
     @Async
     //异步方法
-    public void insert(int i){
-        System.out.println("开始插入--------"+i);
-        int k =0;
-        while (k<100){
+    public void insert(int i) {
+        logger.info("开始插入········");
+        int k = 0;
+        while (k < 100) {
             operatorImpl.addPerson(new Person());
             k++;
         }
-
     }
 }
--- src/main/java/com/xgy/elasticsearchjava/web/WebController.java
+++ src/main/java/com/xgy/elasticsearchjava/web/WebController.java
@@ -19,7 +19,7 @@ public class WebController {
         /*AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AsyncTaskConfig.class);
         AsyncTaskService asyncTaskService = context.getBean(AsyncTaskService.class);*/
 
-        //创建了50个线程
+        //创建了16个线程
         for (int i = 1; i <= 16; i++) {
             asyncTaskService.insert(i);
         }
