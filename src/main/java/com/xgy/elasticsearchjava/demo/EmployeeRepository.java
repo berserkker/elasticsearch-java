db8a3875f0ed64e7b2a263e0ec514808a3739fc6
justxgy <justxgy@outlook.com>
2018-09-25 20:31:21 +0800

    删除部分示例

--- src/main/java/com/xgy/elasticsearchjava/demo/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xgy.elasticsearchjava.demo;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface EmployeeRepository extends ElasticsearchRepository<Employee, String> {
-
-}
